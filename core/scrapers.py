81154e714bb87d2eb36093dadc19c113343fdae5
mattfreire <freire1996@gmail.com>
2020-04-25 13:05:38 +0200

    Scraping with selenium

--- core/scrapers.py
+++ core/scrapers.py
@@ -1,12 +1,50 @@
-import requests
-import time
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 
 def scrape(url):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    time.sleep(10)
-    results = soup.find_all(_class="single-article single-article-small-pic")
-    print(results)
-    return soup
+    options = webdriver.ChromeOptions()
+    options.add_argument(" - incognito")
+
+    browser = webdriver.Chrome(
+        executable_path='./chromedriver', chrome_options=options)
+
+    browser.get(url)
+
+    timeout = 10
+
+    try:
+        WebDriverWait(browser, timeout).until(
+            EC.visibility_of_element_located(
+                (By.XPATH,
+                 "//div[@class='single-article single-article-small-pic']")
+            )
+        )
+    except TimeoutException:
+        print("Timed out waiting for page to load")
+        browser.quit()
+
+    # find all the elements with this class -> single-article single-article-small-pic
+    article_elements = browser.find_elements_by_xpath(
+        "//div[@class='single-article single-article-small-pic']")
+
+    for article in article_elements:
+        # check if div is not a user
+        if article.get_attribute('data-content-user-id') != "undefined":
+
+            # try get the anchor tag and href
+            result = article.find_element_by_xpath(
+                ".//a[@class='small-pic-link-wrapper index-article-link']")
+            news_item_link = result.get_attribute('href')
+            news_item_title = result.text
+
+            # try get title
+            # title_result = article.find_element_by_tag_name('h3')
+
+            # try get timestamp
+            timestamp_result = article.find_element_by_tag_name('time')
+            news_item_time = timestamp_result.text
+            print(news_item_time)
